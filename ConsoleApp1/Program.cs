7656d057dbcdee237af805b07a914807a83e326e
Andy Weekes <userthreethreefive@gmail.com>
2019-02-08 16:57:32 -0700

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the guess box. Will you guess poorly or well?");
+
+
+            bool firstRiddleSolved = false;
+            while (firstRiddleSolved == false)
+            {
+                Console.Write("Guess a number: ");
+                try
+                {
+                    float weight = Convert.ToSingle(Console.ReadLine());
+                    if (weight == 12) firstRiddleSolved = true;
+                    else Console.WriteLine("WRONG");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That was an exceptionally bad guess");
+                }
+            }
+            Console.WriteLine("You nailed it!");
+
+            bool secondRiddleSolved = false;
+            do
+            {
+                Console.Write("Now guess the next number: ");
+                try
+                {
+                    float weight = Convert.ToSingle(Console.ReadLine());
+                    if (weight == 7) secondRiddleSolved = true;
+                    else Console.WriteLine("Nope!");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That was an exceptionally bad guess");
+                }
+            }
+            while (!secondRiddleSolved);
+
+            Console.WriteLine("Nailed it again! Wow, you're *really* good at this.");
+
+            Console.Read();
+        }
+
+    }
+}
